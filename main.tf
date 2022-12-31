7e6d1b4bbfd1baf76882f98d61fc42cbd16ffbec
shenba2014 <luojunwenbashen@163.com>
2022-12-31 15:22:06 +0800

    refine vpc id

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -32,8 +32,9 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
+  subnet_id          = module.blog_vpc.public_subnets[0]
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
@@ -47,7 +48,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog"
 
-  vpc_id = module.vpc.default_vpc_id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
